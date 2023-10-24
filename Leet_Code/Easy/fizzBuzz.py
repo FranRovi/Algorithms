52ee19108598e5edbb8b207e973b75fe011a8a0e
Francisco Rovirosa <rovirosafrancisco@gmail.com>
2023-10-24 09:11:26 -0300

    Add Leet Code Algo 412

---
+++ Leet_Code/Easy/fizzBuzz.py
@@ -0,0 +1,21 @@
+# Leet Code 412. Fizz Buzz
+
+def fizzBuzz(n):
+    answer = []
+    for i in range(1, n+1):
+        if i % 3 == 0  and i % 5 == 0:
+            answer.append("FizzBuzz")
+        elif i % 5 == 0:
+            answer.append("Buzz")
+        elif i % 3 == 0:
+            answer.append("Fizz")
+        else:
+            answer.append(str(i))
+    return answer
+
+
+print(fizzBuzz(3))
+print(fizzBuzz(5))
+print(fizzBuzz(15))
+    
+    
