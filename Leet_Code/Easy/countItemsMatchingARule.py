f08bb345d0dac22bd6bc4f27889da1dafca4dfa8
Francisco Rovirosa <rovirosafrancisco@gmail.com>
2023-11-01 20:17:32 -0300

    Remove blank lines and print statements from Algo 1773

--- Leet_Code/Easy/countItemsMatchingARule.py
+++ Leet_Code/Easy/countItemsMatchingARule.py
@@ -12,14 +12,6 @@ def countItemsMatchingARule(items, ruleKey, ruleValue):
         else:
             if items[i][2] == ruleValue:
                 counter +=1
-            # print(items[1][i])
-        # if items[0][i] == 'type' and items[i][0] == ruleValue:
-        #     counter += 1
-        # elif items[1][i] == 'color' and items[i][1] == ruleValue:
-        #     counter += 1
-        # else:
-        #     if items[2][i] == ruleValue:
-                counter += 1
     return counter
 
 print(countItemsMatchingARule([["phone", "blue", "pixel"], ["computer", "silver", "lenovo"],["phone", "gold", "iphone"]], "color", "silver"))
