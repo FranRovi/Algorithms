f842e58195ed65e17a1d280e26b1457bb81f3dc9
Francisco Rovirosa <rovirosafrancisco@gmail.com>
2023-11-23 12:32:44 -0300

    Delete print statements

--- Leet_Code/Easy/checkIfAStringIsAnAcronymOfWords.py
+++ Leet_Code/Easy/checkIfAStringIsAnAcronymOfWords.py
@@ -1,11 +1,9 @@
 # Leet Code Algo 2828. Check if a string is an Acronym Of Words
 
 def checkIfAStringIsAnAcronymOfWords(words, s):
-    # print(len(words), len(s))
     if len(words) != len(s):
         return False
     for i in range(len(words)):
-        # print(words[i][0], s[i])
         if words[i][0] != s[i]:
             return False
     return True
