3e67bb2ee2942c49dcd3bbd109bca649fb53ca65
Francisco Rovirosa <rovirosafrancisco@gmail.com>
2023-12-20 10:44:35 -0300

    Add Leet Code Algo 344

---
+++ Leet_Code/Easy/reverseString.py
@@ -0,0 +1,14 @@
+# Leet Code Algo 344. Reverse String
+
+def  reverseString(s):
+    rightIdx = -1
+    for i in range(int(len(s)/2)):
+        temp = s[i]
+        s[i] = s[rightIdx]
+        s[rightIdx] = temp
+        rightIdx -= 1
+    print(s)
+
+
+reverseString(["h","e","l","l","o"])
+reverseString(["H","a","n","n","a","h"])
