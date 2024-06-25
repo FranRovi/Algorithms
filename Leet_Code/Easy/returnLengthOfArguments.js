4c35b313381b5e38e32cfc514d3a0b9433e51de7
Francisco Rovirosa <rovirosafrancisco@gmail.com>
2024-06-25 20:54:38 -0300

    Add Leet Code Algo 2703

---
+++ Leet_Code/Easy/returnLengthOfArguments.js
@@ -0,0 +1,10 @@
+// Leet Code Algo 2703. Return Length Of Arguments Passed
+
+/**
+ * @param {...(null|boolean|number|string|Array|Object)} args
+ * @return {number}
+ */
+var argumentsLength = function(...args) {
+    return arguments.length
+    
+};
