1fcc66452532820accb38f45751f27be5aff9efa
Francisco Rovirosa <rovirosafrancisco@gmail.com>
2024-07-25 21:03:29 -0300

    Add Leet Code Algo 1464

--- Leet_Code/Easy/maximumProductOfTwoElementsInAnArray.py
+++ Leet_Code/Easy/maximumProductOfTwoElementsInAnArray.py
@@ -1,13 +1,30 @@
 # Leet Code Algo 1464. Maximum Product of Two Elements in an Array
 
 def maxProduct(nums):
-    maxTotal = 0
-    for i in range(len(nums)-1):
-        for j in range(i+1,len(nums)):
-            if (nums[i]-1) * (nums[j]-1) > maxTotal:
-                maxTotal = (nums[i]-1) * (nums[j]-1)
-    print(maxTotal)
+    if len(nums) == 2:
+        return (nums[0] - 1) * (nums[1] -1)
+    if nums[0] > nums[1]:
+        largest = nums[0]
+        largestIdx = 0
+        secondLargest = nums[1]
+        secondLargestIdx = 1
+    else:
+        largest = nums[1]
+        largestIdx = 1
+        secondLargest = nums[0]
+        secondLargestIdx = 0
+    for i in range(2, len(nums)):
+        if nums[i] > largest:
+            secondLargest = largest
+            secondLargestIdx = largestIdx
+            largest = nums[i]
+            largestIdx = i
+        else:
+            if nums[i] > secondLargest:
+                secondLargest = nums[i]
+                secondLargestIdx = i
+    return (nums[largestIdx] - 1) * (nums[secondLargestIdx] -1)   
 
-maxProduct([3,4,5,2])
-maxProduct([1,5,4,5])
-maxProduct([3,7])
+print(maxProduct([3,4,5,2]))
+print(maxProduct([1,5,4,5]))
+print(maxProduct([3,7]))
