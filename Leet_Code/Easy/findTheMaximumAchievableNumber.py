dd0098ff10fa9e7cf93bb0e95b112c61a61c660f
Francisco Rovirosa <rovirosafrancisco@gmail.com>
2024-08-05 17:33:40 -0300

    Add Leet Code Algo 2769

---
+++ Leet_Code/Easy/findTheMaximumAchievableNumber.py
@@ -0,0 +1,7 @@
+# Leet Code Algo 2769. Find The MaximumAchievable Number
+
+def findTheMaximumAchievableNumber(num, t):
+    return num + 2 * t
+
+print(findTheMaximumAchievableNumber(4, 1))
+print(findTheMaximumAchievableNumber(3, 2))
