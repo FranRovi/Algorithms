dad65aac8eb5661f8321d8882fb2391a733249eb
Francisco Rovirosa <rovirosafrancisco@gmail.com>
2025-01-24 23:40:43 -0800

    Add Leet Code SQL Algo 1113

---
+++ Leet_Code/Easy/reportedPosts.sql
@@ -0,0 +1,7 @@
+-- Leet Code SQL Algo 1113. Reported Posts
+
+SELECT extra AS report_reason, COUNT( DISTINCT (post_id) ) AS report_count
+FROM Actions
+WHERE action = 'report' AND action_date = '2019-07-04'
+GROUP BY report_reason
+ORDER BY report_reason;
