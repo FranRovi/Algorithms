cea20350daffe6b64791ab2e62bdba2d1c54d59d
Francisco Rovirosa <rovirosafrancisco@gmail.com>
2025-09-17 23:59:03 -0700

    Add Leet Code Algo 2869

---
+++ Leet_Code/Easy/minimumOperationsToCollectAllElements.py
@@ -0,0 +1,19 @@
+# Leet Code Algo . Minimum Time To Collect all Elements
+
+def minOperations(nums, k):
+    target_arr = []
+    for i in range(1,k+1):
+        target_arr.append(i)
+    counter = 0
+    for j in range(len(nums)-1, -1,-1):
+        if len(target_arr) == 0:
+            break
+        else:
+            counter += 1
+            if nums[j] in target_arr:
+                target_arr.remove(nums[j])
+    return counter
+
+print(minOperations([3,1,5,4,2], 2))
+print(minOperations([3,1,5,4,2], 5))
+print(minOperations([3,2,5,3,1], 3))
